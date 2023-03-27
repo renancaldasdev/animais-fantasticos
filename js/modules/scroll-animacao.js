159f563ce0d0a2bfbe942cc945f24546d45f008a
Renan Caldas <renan@renancaldas.dev.br>
2023-03-27 18:26:29 -0300

    fix: correção de scroll

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -1,16 +1,18 @@
 export default function initAnimacaoScroll() {
-  const sections = document.querySelectorAll("[data-anime='scroll']");
-  sections[0].classList.add("ativo");
+    const sections = document.querySelectorAll("[data-anime='scroll']");
+    sections[0].classList.add("ativo");
 
-  if (sections.length) {
-    function animaScroll() {
-      sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top - 800;
-        if (sectionTop < 0) {
-          section.classList.add("ativo");
+    if (sections.length) {
+        function animaScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top - 800;
+                if (sectionTop < 0) {
+                    section.classList.add("ativo");
+                } else if (section.classList.contains("ativo")) {
+                    section.classList.remove("ativo");
+                }
+            });
         }
-      });
+        window.addEventListener("scroll", animaScroll);
     }
-    window.addEventListener("scroll", animaScroll);
-  }
 }
