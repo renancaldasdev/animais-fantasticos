595501d5f95e1149e31af77897016331644b370f
Renan Caldas <renan@renancaldas.dev.br>
2023-04-18 10:29:53 -0300

    refactot: Accordion transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 import ScrollSuave from "./modules/scroll-suave.js";
+import Accordion from "./modules/accordion-animacao.js";
+
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
-import initAccordion from "./modules/accordion-animacao.js";
 import initTabNav from "./modules/tabMenu-animacao.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
@@ -13,8 +14,10 @@ import initFetchBitcoin from "./modules/fetchBitcoin.js";
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
 initAnimacaoScroll();
-initAccordion();
 initTabNav();
 initModal();
 initTooltip();
